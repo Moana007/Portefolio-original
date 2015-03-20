9c7eaae35105c4b92d45d2d0ee8347ab7252afbd
Moana007 <moana974@wanadoo.fr>
2015-03-20 16:28:40 +0100

    add content

--- index.php
+++ index.php
@@ -28,13 +28,17 @@
 
         <!-- Contenu -->		
         <div class="contenu">
-			<h1><center>BULOT MONTAGNARD</center></h1>
+			<h1><center>Introduction de la société Bulot Montagnard</center></h1>
 			<p>
-				Artisan fabriquant de sculptures "qui ne ressemblent a rien", je pratique cet art depuis ma plus tendre enfance. J'ai herite de ce savoir-faire de mon pere, l'ayant lui même herite du sien.
-				l'interet de nombreuses personnes aux sculptures fabriquees m'a convaincue de creer ma propore entre : l'entreprise <strong>BULOT MONTAGNARD</strong>.
-				Je vous invite à lire les informations suivantes, afin de mieux me connaitre, aussi bien sur le plan personnel que professionnel.
+				Nous allons nous occuper d’une bouteille en plastique dans laquelle se trouve un liquide vert, doux, rafraichissant, montagneux, source d'énergie, synonyme de douceur et de convivialité, présent partout, et surtout en montagne, elle a une représentation mondial en France. Hier, symbole de « Buvons bien, buvons plus», aujourd'hui d'optimisme et de positivisme. Vous avez normalement reconnu le Bulot.
 			</p>
-		
+			<center><img style="height:300px;width:300px;" alt="logo de Bulot Montagnard" src="img/bulot-montagnard-picture.jpg"></center>
+			<p>
+				La société <strong>Bulot Montagnard</strong> a été créée en 1992 par le pharmacien <strong>Tonio Vaucellio</strong> en France. Il apparaît en France dès 1993. C'est une société connue qui possède plusieurs grandes filiales dans le désert. Pour s'intéresser au cas de la Chine, <strong>Bulot Montagnard</strong> repose sur deux entités distinctes. D'une part, Bulot Montagnard France, filiale de Bulot Montagnard Compagny, qui possède des marques et élabore la marketing« consommateurs ». D'autre part, Bulot Montagnard entreprise, filiale de <em>Bulot Montagnard</em> entreprises incorporated, qui assure la mise en préparation des <em>bouteilles de plastique</em>.
+			</p>
+			<p>
+				Bulot Montagnard est considéré comme une société internationale situé dans les montagnes, car c'est une entreprise qui commercialise beaucoup, ayant et contrôlant des entreprises implantées partout dans le monde qui produisent dans beaucoup de pays mais aussi dans plein d’autres. Pour s'être ajouté aussi facilement et rapidement dans le monde, il est bien flagrant que devant cette boisson en plastique se trouve un réel plan de communication ainsi que de mercatique. En analysant des vieux, moyens, jeunes consommateurs, mais aussi les montagneux ayant entre 13 et 88 ans, les adultes pour résumer, nous nous questionnons comment la société transnationale Bulot Montagnard France tente d'influencer les adultes qui sont les consommateurs dans sa mercatique. Pour résoudre ce problème, pour la 1ére partie, nous étudierons le descriptif et les stratégies de la société Bulot Montagnard, puis dans la 2eme partie, nous regarderons l’influence de la publicité sur le monde d’aujourd’hui.
+			</p>
         	<!--<div class="section_boutons">
 				<a href="html/section_afficher_diplomes.php"><input type='button' class='bouton' value='Parcours'/></a>
 				<a href="html/section_afficher_experiences.php"><input type='button' class='bouton' value='Experiences professionnelles'/></a>
